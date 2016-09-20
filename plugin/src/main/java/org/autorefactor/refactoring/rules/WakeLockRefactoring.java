b2d5d40a08326bc084f667230def6664a30f1e27
Luis Cruz <ei06034@fe.up.pt>
2016-09-20 13:21:56 +0100

    first attemp for a Refactoring Rule -- test is failing; added android dependency to maven

---
+++ plugin/src/main/java/org/autorefactor/refactoring/rules/WakeLockRefactoring.java
@@ -0,0 +1,79 @@
+/*
+ * AutoRefactor - Eclipse plugin to automatically refactor Java code bases.
+ *
+ * Copyright (C) 2013-2016 Jean-Noël Rouvignac - initial API and implementation
+ * Copyright (C) 2016 Fabrice Tiercelin - Make sure we do not visit again modified nodes
+ * Copyright (C) 2016 Luis Cruz - Android Refactoring
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program under LICENSE-GNUGPL.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution under LICENSE-ECLIPSE, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.autorefactor.refactoring.rules;
+
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.MethodInvocation;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.internal.corext.dom.ASTNodes;
+
+import static org.autorefactor.refactoring.ASTHelper.*;
+import static org.eclipse.jdt.core.dom.ASTNode.*;
+
+/** See {@link #getDescription()} method. */
+public class WakeLockRefactoring extends AbstractRefactoringRule {
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return "Failing to release a wakelock properly can keep the Android device in a high "
+				+ "power mode, which reduces battery life. There are several causes of this, such "
+				+ "as releasing the wake lock in onDestroy() instead of in onPause(), failing to "
+				+ "call release() in all possible code paths after an acquire(), and so on.";
+	}
+
+	@Override
+	public String getName() {
+		return "WakeLockRefactoring";
+	}
+
+	@Override
+    public boolean visit(MethodInvocation node) {
+		if(isMethod(node, "android.os.PowerManager.WakeLock", "release")){
+			// check whether it is being called in onDestroy
+			MethodDeclaration enclosingMethod = (MethodDeclaration) ASTNodes.getParent(node, ASTNode.METHOD_DECLARATION);
+			String enclosingMethodName = enclosingMethod.getName().getFullyQualifiedName();
+			if("android.app.Activity.onUpdate".equals(enclosingMethodName)){
+				System.out.println("Good");
+			}
+			else if("android.app.Activity.onDestroy".equals(enclosingMethodName)){
+				System.out.println("Issue");
+				ctx.getRefactorings().set(node, MethodInvocation.NAME_PROPERTY, "estaAFuncionar");
+			}
+			else{
+				System.out.println("Found nothing:"
+						+ enclosingMethodName);
+			}
+			
+			
+			// put it on onPause
+			return DO_NOT_VISIT_SUBTREE;
+		}
+        return VISIT_SUBTREE;
+    }
+}
---
+++ samples/src/test/java/org/autorefactor/refactoring/rules/samples_in/WakeLockSample.java
@@ -0,0 +1,32 @@
+package org.autorefactor.refactoring.rules.samples_in;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+
+public class WakeLockSample extends Activity {
+	private PowerManager.WakeLock wl;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+
+		PowerManager pm = (PowerManager)this.getSystemService(Context.POWER_SERVICE);
+		WakeLock wl = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE, "WakeLockSample");
+		wl.acquire();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+	
+	@Override
+	public void onDestroy(){
+		wl.release();
+		super.onDestroy();
+	}
+	
+}
