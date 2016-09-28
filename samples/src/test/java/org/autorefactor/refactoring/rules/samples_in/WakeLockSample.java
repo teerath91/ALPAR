79085b310ae5e392b3c895ebadf04be31655785d
Luis Cruz <ei06034@fe.up.pt>
2016-09-28 15:17:07 +0100

    sample for new wake lock rule - when there is no release, add one

--- samples/src/test/java/org/autorefactor/refactoring/rules/samples_in/WakeLockSample.java
+++ samples/src/test/java/org/autorefactor/refactoring/rules/samples_in/WakeLockSample.java
@@ -50,5 +50,23 @@ public class WakeLockSample {
 			super.onDestroy();
 		}
 	}
+	
+	public class SimpleWakeLockWithoutReleaseActivity extends Activity {
+		private PowerManager.WakeLock wl;
+		
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+			// TODO Auto-generated method stub
+			super.onCreate(savedInstanceState);
+
+			PowerManager pm = (PowerManager)this.getSystemService(Context.POWER_SERVICE);
+			WakeLock wl = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE, "WakeLockSample");
+			wl.acquire();
+		}
+
+		@Override() protected void onPause(){
+		super.onPause();
+		}
+	}
 }
 
